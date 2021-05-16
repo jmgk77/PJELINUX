5ccc0ffb2b1c91aed05ce0f5d9207ec314fb7c9c
JMGK <jmgk77@gmail.com>
2021-05-16 19:08:51 -0300

    suporte para 16.04

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Sistemas testados:
 * Ubuntu 20.04
 * Ubuntu 18.04
+* Ubuntu 16.04
 
 ## Instruções:
 
--- instalar.sh
+++ instalar.sh
@@ -20,7 +20,9 @@ fi
 
 #instala drivers do smartcard 
 sudo apt update && sudo apt upgrade
-sudo apt install libengine-pkcs11-openssl libp11-3 libpcsc-perl libccid libpcsclite1 pcscd pcsc-tools libasedrive-usb opensc openssl libengine-pkcs11-openssl
+sudo apt install libengine-pkcs11-openssl libpcsc-perl libccid libpcsclite1 pcscd pcsc-tools libasedrive-usb opensc openssl libengine-pkcs11-openssl
+#nao existe no ubuntu 16.04...
+#sudo apt install libp11-3 
 
 #extrai bibliotecas para a pasta do sistema
 sudo tar -xf data.tgz -C /usr/lib --exclude='pjeOffice.desktop'
@@ -40,9 +42,12 @@ link_if_not_exists /usr/lib/libaetdlss.so.3.0.2508 /usr/lib/libaetdlss.so
 sudo chown $USER:$USER /usr/lib/libaetdlss.so.3.0.2508 /usr/lib/libaetjcss.so.3.0.2509 /usr/lib/libaetpkss.so.3.0.2528
 
 #baixa libs que não estão disponiveis via apt-get
+
+#Ubuntu 16.04 tem libgdbm3 disponivel via apt-get
 wget http://ftp.br.debian.org/debian/pool/main/g/gdbm/libgdbm3_1.8.3-14_amd64.deb
 
 #Ubuntu 18.04 tem libssl1.0.0 disponivel via apt-get
+#Ubuntu 16.04 tem libssl1.0.0 disponivel via apt-get
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.6_amd64.deb
 
 #baixa pjeOffice
