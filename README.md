c4cdca87fa7c09b297b4a3ffce94a91ab8583aa2
JMGK <jmgk77@gmail.com>
2021-05-16 18:33:54 -0300

    suporte para 18.04

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 ## Sistemas testados:
 * Ubuntu 20.04
+* Ubuntu 18.04
 
 ## Instruções:
 
 Coloque `instalar.sh` e `data.tgz` na mesma pasta e execute `instalar.sh`. Isso vai copiar as bibliotecas para o sistema, instalar suas dependencias, instalar o PjeOffice e criar um atalho.
 
-Entre sua senha de root quando perguntado.
+Entre sua senha de root quando perguntado. Algumas vezes, o PjeOffice não consegue detectar automaticamente a biblioteca, e pode ser necessário escolhe-la manualmente (`/usr/lib/libaet???.so`).
--- instalar.sh
+++ instalar.sh
@@ -20,7 +20,7 @@ fi
 
 #instala drivers do smartcard 
 sudo apt update && sudo apt upgrade
-sudo apt install libengine-pkcs11-openssl libp11-3 libpcsc-perl libccid libpcsclite1 pcscd pcsc-tools libasedrive-usb opensc openssl libengine-pkcs11-openssl #default-jre
+sudo apt install libengine-pkcs11-openssl libp11-3 libpcsc-perl libccid libpcsclite1 pcscd pcsc-tools libasedrive-usb opensc openssl libengine-pkcs11-openssl
 
 #extrai bibliotecas para a pasta do sistema
 sudo tar -xf data.tgz -C /usr/lib --exclude='pjeOffice.desktop'
@@ -39,8 +39,10 @@ link_if_not_exists /usr/lib/libaetdlss.so.3.0.2508 /usr/lib/libaetdlss.so
 #conserta permisssões
 sudo chown $USER:$USER /usr/lib/libaetdlss.so.3.0.2508 /usr/lib/libaetjcss.so.3.0.2509 /usr/lib/libaetpkss.so.3.0.2528
 
-#baixa libcrypto.so.1.0.0 e libgdbm_compat.so.3 (que não estão disponiveis via apt-get)
+#baixa libs que não estão disponiveis via apt-get
 wget http://ftp.br.debian.org/debian/pool/main/g/gdbm/libgdbm3_1.8.3-14_amd64.deb
+
+#Ubuntu 18.04 tem libssl1.0.0 disponivel via apt-get
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.6_amd64.deb
 
 #baixa pjeOffice
